636ff27e16b1f1045df79146d101c3d73ac42485
Shubham Biyani <shubhambiyani10@gmail.com>
2023-03-21 15:52:26 +0530

    small changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,28 @@
 /**
- * @type {import('gatsby').GatsbyConfig}
+ * 
  */
+// module.exports = {
+//   siteMetadata: {
+//     siteUrl: `https://www.yourdomain.tld`,
+//   },
+//   plugins: [],
+// }
+
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
+    title: "My Gatsby Blog Site",
   },
-  plugins: [],
-}
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `Blog`,
+        path: `${__dirname}/Blog`,
+      }
+    },
+    "gatsby-plugin-mdx",
+  ],
+};
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,14 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import Seo from '../components/seo'
 
 const AboutPage = () => {
   return (
-    <Layout pageTitle="About Me">
+    <Layout pageTitle="About Us">
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
     </Layout>
   )
 }
 
-export const Head = () => <title>About Me</title>
-
+export const Head = () => <Seo title="About Us"/>
 export default AboutPage
